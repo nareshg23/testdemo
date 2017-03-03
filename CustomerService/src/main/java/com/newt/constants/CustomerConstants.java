c154bc29e3907c46503bb9b767bf6e223a64c67b
nareshg23 <nareshg23@users.noreply.github.com>
2017-03-03 16:40:41 +0530

    Update CustomerConstants.java

--- CustomerService/src/main/java/com/newt/constants/CustomerConstants.java
+++ CustomerService/src/main/java/com/newt/constants/CustomerConstants.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+
+
 @Configuration
 @ComponentScan
 public class CustomerConstants {
